64ab21cd8a603dc30f3e7a2933d2da5ae0970fb4
anissarkeraraf <anissarkeraraf@gmail.com>
2024-06-04 09:19:22 +0600

    added the get of donor Request collection

--- index.js
+++ index.js
@@ -73,6 +73,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/donorRequest', async (req, res) => {
+      const cursor = donerRequestCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
